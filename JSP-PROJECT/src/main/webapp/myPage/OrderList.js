aaf3af16236ee20cd4f4e038fb3c765fae8b7458
namung08 <namung08@gmail.com>
2024-03-20 10:39:52 +0900

    주문목록에서 리뷰작성 버튼을 눌렀을 시에 isbn을 파라미터로 넘기게 함

--- JSP-PROJECT/src/main/webapp/myPage/OrderList.js
+++ JSP-PROJECT/src/main/webapp/myPage/OrderList.js
@@ -5,6 +5,15 @@ $(document).ready(function() {
 	$('.btn').click(function() {
 		var $tr = $(this).closest('tr');
 		var isbn = $tr.find('input[name="isbn"]').val();
+		var form = $('<form></form>');
+		// form 속성을 설정합니다.
+		form.attr('method', 'post');
+		form.attr('action', '/book/insertReply.bo');
+		// 필드 생성 및 값 설정
+		form.append('<input type="hidden" name="isbn" value="' + isbn + '">');
+		// form을 body에 추가하고 submit 합니다.
+		$('body').append(form);
+		form.submit();
 	})
 });
 
