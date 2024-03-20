2b29c4ce70de23cdeced1050eb0728253481e966
namung08 <namung08@gmail.com>
2024-03-20 15:08:58 +0900

    오더넘 넘기기 완료

--- JSP-PROJECT/src/main/webapp/myPage/OrderList.js
+++ JSP-PROJECT/src/main/webapp/myPage/OrderList.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 		var $tr = $(this).closest('tr');
 		var flag = $tr.find('input[name="flag"]').val();
 		var isbn = $tr.find('input[name="isbn"]').val();
+		var ordernum = $tr.find('input[name="ordernum"').val();
 		var form = $('<form></form>');
 		if(flag !== "리뷰작성"){
 			// form 속성을 설정합니다.
@@ -13,6 +14,7 @@ $(document).ready(function() {
 			form.attr('action', '/book/writeReply.bo');
 			// 필드 생성 및 값 설정
 			form.append('<input type="hidden" name="isbn" value="' + isbn + '">');
+			form.append('<input type="hidden" name="ordernum" value="' + ordernum + '">');
 			// form을 body에 추가하고 submit 합니다.
 			$('body').append(form);
 			form.submit();
