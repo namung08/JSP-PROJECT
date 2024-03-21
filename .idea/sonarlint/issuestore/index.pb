f42cdb57ae2daa0d812d54c9f665b7866d729210
namung08 <namung08@gmail.com>
2024-03-21 11:42:42 +0900

    책 상세 페이지에서 바로 구매시 오류가 발생하는 점 수정

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+
+OJSP-PROJECT/src/main/java/com/KSW/web/servlet/Action/InsertOrderListAction.java,b\6\b6f188661c21b12ca77e9d753e6c89c78f384d38
--- JSP-PROJECT/src/main/java/com/KSW/web/servlet/Action/InsertOrderListAction.java
+++ JSP-PROJECT/src/main/java/com/KSW/web/servlet/Action/InsertOrderListAction.java
@@ -1,17 +1,16 @@
 package com.KSW.web.servlet.Action;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import com.KSW.web.action.Action;
 import com.KSW.web.action.ActionForward;
 import com.KSW.web.dao.CartDAO;
 import com.KSW.web.dao.OrderlistDAO;
 import com.KSW.web.dto.OrderListDTO;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class InsertOrderListAction implements Action{
 
@@ -21,18 +20,21 @@ public class InsertOrderListAction implements Action{
 		OrderListDTO odto = new OrderListDTO();
 		OrderlistDAO odao = new OrderlistDAO();
 		CartDAO cdao = new CartDAO();
-		
+
 		HttpSession session = req.getSession();
 		String userid = (String)session.getAttribute("userid");
-		int cartnum = Integer.parseInt(req.getParameter("cartNum"));
-		System.out.println(cartnum);
-		cdao.deleteCart(cartnum);
+		String cartnum = req.getParameter("cartNum");
+		if(cartnum != null && !cartnum.isEmpty()) {
+			int cnum = Integer.parseInt(cartnum);
+			System.out.println(cnum);
+			cdao.deleteCart(cnum);
+		}
 		String title = req.getParameter("title");
 		String discount = req.getParameter("discount");
 		System.out.println(discount);
 		String quantity = req.getParameter("quantity");
 		int count;
-		if(quantity != null && quantity != "") {
+		if(quantity != null && !quantity.isEmpty()) {
 			count = Integer.parseInt(quantity);
 			odto.setCount(count);
 		} else {
@@ -43,18 +45,18 @@ public class InsertOrderListAction implements Action{
 		discount = discount.trim().replace(",", "").replace("원", "");
 		int dis = Integer.parseInt(discount);
 		dis = dis*count;
-		
+
 		odto.setUserId(userid);
-		
+
 		LocalDate currentDate = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String formattedDate = currentDate.format(formatter);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		String formattedDate = currentDate.format(formatter);
 		odto.setIsbn(isbn);
 		odto.setOrderDate(formattedDate);
 		odto.setTitle(title);
 		odto.setDiscount(dis);
 		System.out.println(odto.getIsbn());
-		
+
 		if(odao.insertOrderList(odto)) {
 			System.out.println("성공!");
 			forward.setPath(req.getContextPath()+"/myPage/OrderList.bo");
@@ -62,7 +64,7 @@ public class InsertOrderListAction implements Action{
 		}
 		return forward;
 	}
-	
-	
+
+
 
 }
